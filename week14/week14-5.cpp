6ab3af33843c9bfe857fa3f8b6220e1362b638c3
jy796 <g0909626138@gmail.com>
2025-12-10 11:53:25 +0800

    week14

---
+++ week14/week14-5.cpp
@@ -0,0 +1,15 @@
+/// week14-5.cpp
+/// ¬Q¤Ñ CPE ²Ä1ÃD UVA 10079
+/// Pizza Cutting ¤ÁPizza
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    while ( cin >> N ) {
+        if (N<0) break;
+        int ans = 1 + (1+N)*N/2;
+        cout << ans << "\n";
+    }
+}
